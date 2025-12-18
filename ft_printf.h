daf6101cbeed0846c3abbcedbf49f6202ef2d356
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2025-12-18 19:43:25 +0800

    feat: add libft functions for handlers

--- ft_printf.h
+++ ft_printf.h
@@ -6,16 +6,16 @@
 /*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 19:52:43 by rfoo              #+#    #+#             */
-/*   Updated: 2025/12/12 16:26:36 by rfoo             ###   ########.fr       */
+/*   Updated: 2025/12/18 19:39:30 by rfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+# include <unistd.h>
+# include <stdlib.h>
 # include <stdarg.h>
-# include "libft/libft.h"
-
 typedef struct entry
 {
 	char	key;
@@ -29,7 +29,14 @@ typedef struct dict
 }			t_dict;
 
 int		ft_printf(const char *format, ...);
-int		*insert();
-void	*lookup_dict();
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+void	handle_char(char c);
+void	handle_str(char *s);
+void	handle_int(int n);
+void	handle_ptr(void *ptr);
+// void	handle_uint(unsigned int n);
 
 #endif
---
+++ ft_putnbr_fd.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 17:29:20 by rfoo              #+#    #+#             */
+/*   Updated: 2025/12/18 19:39:12 by rfoo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned long nbr;
+	size_t
+
+	nbr = n;
+	if (nbr == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (nbr < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = -n;
+	}
+	if (nbr >= 10)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd((nbr % 10) + '0', fd);
+}
