27014b0fd9cfa236ae42031419e2676ca4da10dd
Russell Song Jie Foo <rfoo@c2r7s4.42singapore.sg>
2026-01-20 16:33:17 +0800

    fix: fix casting for handle lower/hyperhex when fetching from va_list

--- handle_lowerhex.c
+++ handle_lowerhex.c
@@ -6,7 +6,7 @@
 /*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 17:48:58 by rfoo              #+#    #+#             */
-/*   Updated: 2026/01/20 12:14:18 by rfoo             ###   ########.fr       */
+/*   Updated: 2026/01/20 16:24:36 by rfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	handle_lowerhex(va_list *args)
 {
-	unsigned long	n;
+	unsigned int	n;
 
-	n = va_arg(*args, unsigned long);
-	return (print_hex(n, 0));
+	n = va_arg(*args, unsigned int);
+	return (print_hex((unsigned long)n, 0));
 }
--- handle_upperhex.c
+++ handle_upperhex.c
@@ -6,7 +6,7 @@
 /*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 17:49:09 by rfoo              #+#    #+#             */
-/*   Updated: 2026/01/20 12:14:26 by rfoo             ###   ########.fr       */
+/*   Updated: 2026/01/20 16:29:21 by rfoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	handle_upperhex(va_list *args)
 {
-	unsigned long	n;
+	unsigned int	n;
 
-	n = va_arg(*args, unsigned long);
-	return (print_hex(n, 1));
+	n = va_arg(*args, unsigned int);
+	return (print_hex((unsigned long)n, 1));
 }
